5580d45edc17c8e4597e224745d89e254306a9b1
Olha Khomlyak <olga.khomlyak@gmail.com>
2021-09-07 23:17:56 +0300

    native stack navigation installed

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>HOME</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default Home;
