87c9f8f5471f0f8d3157e6ac578f6e54b254f02d
Olha Khomlyak <olga.khomlyak@gmail.com>
2021-09-08 13:32:14 +0300

    get list of episodes

--- src/appstate/actions/ApiCalls.js
+++ src/appstate/actions/ApiCalls.js
@@ -5,16 +5,19 @@ import {
     EPISODE_LIST
 } from './type';
 
-export const getEpisodes = () => (dispatch) => {
+export const getEpisodes = (page) => (dispatch) => {
     return new Promise(async(resolve, reject) => {
         axios(
             {
                 method: 'get',
-                url: 'https://rickandmortyapi.com/api/episode',// 'https://api.employeez.me/GetMyTrophies',
+                url: 'https://rickandmortyapi.com/api/episode',
+                data: {
+                    page: page
+                }
             })
 
             .then((response) => {
-                console.log('getEpisodes===', response.data.results);
+                console.log('getEpisodes===', response);
                 //let list = JSON.parse(response.data.results)
                 dispatch({
                     type: EPISODE_LIST,
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { getEpisodes } from '../appstate/actions/ApiCalls';
+import { connect } from 'react-redux';
 
-const Home = () => {
+const Home = (props) => {
+
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+
+    useEffect(() => {
+        getEpisodesList()
+    }, [])
+
+
+    const getEpisodesList = () => {
+        props.getEpisodes('2').then(
+            () => {
+                setLoading(false)
+            },
+            () => {
+                setError(true)
+            })
+    }
+
+const renderList = ({item}) => {
+    return(
+        <View style={{borderWidth:1, margin:5}}>
+            <Text>{item.name}</Text>
+            <Text>{item.air_date}</Text>
+        </View>
+    )
+}
+
     return (
         <SafeAreaView>
-            <View>
-                <Text>HOME</Text>
-            </View>
+            {error ?
+                <View>
+                    <Text>Ooops something went wrong</Text>
+                </View>
+                :
+                <>
+                    {loading ?
+                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                            <ActivityIndicator size="large" color='grey' />
+                        </View>
+                        :
+                        <View>
+                            <Text>HOME</Text>
+                            <FlatList 
+                            data={props.episodes}
+                            keyExtractor={item => item.id}
+                            renderItem={renderList}
+                            />
+                        </View>}
+                </>
+            }
+
         </SafeAreaView>
     );
 }
 
-export default Home;
+function mapStateToProps(state) {
+    console.log(state);
+    return {
+        episodes: state.ApiReducer.episodeList
+    }
+}
+
+export default connect(mapStateToProps, { getEpisodes })(Home);
